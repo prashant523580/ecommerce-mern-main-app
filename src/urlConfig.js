f85a361854e4541110092682ac878ded2f73607a
root <root_-@2400-1A00-B1C0-ip6.wlink.com.np>
2022-05-10 21:42:42 +0545

    replace apli

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,14 +1,14 @@
-// export const api = "https://mernbackend-server.herokuapp.com/api";
-// export const generateImgUrl = (image) => {
-//     return `https://mernbackend-server.herokuapp.com/public/${image}`;
-// }
+export const api = "https://mernbackend-server.herokuapp.com/api";
+export const generateImgUrl = (image) => {
+    return `https://mernbackend-server.herokuapp.com/public/${image}`;
+}
 
 
 // export const api = "http://192.168.100.74:4545/api";
 // export const generateImgUrl = (image) => {
 //     return `http://192.168.100.74:4545/public/${image}`;
 // }
-export const api = "http://localhost:4343/api";
-export const generateImgUrl = (image) => {
-    return `http://localhost:4343/public/${image}`;
-}
+// export const api = "http://localhost:4343/api";
+// export const generateImgUrl = (image) => {
+//     return `http://localhost:4343/public/${image}`;
+// }
