6b7e1ebcd3113dff9bcebe5db2e85f7d9eb61cb5
prashant523580 <prashant.thapa62758@gmail.com>
2022-05-13 23:16:00 +0545

    replace apis

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,7 @@
 export const api = "https://mernbackend-server.herokuapp.com/api";
 export const generateImgUrl = (image) => {
    return `https://mernbackend-server.herokuapp.com/public/${image}`;
-/}
+}
 
 
 // export const api = "http://192.168.100.66:4343/api";
