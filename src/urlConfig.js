9a4259906e306a72ac974e8e5814e0088eeb0f42
prashant523580 <prashant.thapa62758@gmail.com>
2022-05-13 23:14:48 +0545

    replace apis

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,13 +1,13 @@
-//export const api = "https://mernbackend-server.herokuapp.com/api";
-//export const generateImgUrl = (image) => {
- //   return `https://mernbackend-server.herokuapp.com/public/${image}`;
-//}
+export const api = "https://mernbackend-server.herokuapp.com/api";
+export const generateImgUrl = (image) => {
+   return `https://mernbackend-server.herokuapp.com/public/${image}`;
+/}
 
 
- export const api = "http://192.168.100.66:4343/api";
- export const generateImgUrl = (image) => {
-     return `http://192.168.100.66:4343/public/${image}`;
-}
+// export const api = "http://192.168.100.66:4343/api";
+// export const generateImgUrl = (image) => {
+//     return `http://192.168.100.66:4343/public/${image}`;}
+
 
  //export const api = "http://localhost:4343/api";
  //export const generateImgUrl = (image) => {
